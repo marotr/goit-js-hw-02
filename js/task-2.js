ce7e250ec6402e445b46a3e77f3ccb71c19b384c
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-01-23 21:20:09 +0300

    task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,9 @@
 function formatMessage(message, maxLength) {
-    if (message <= maxLength) {
+    if (message.length <= maxLength) {
         return message;
     }
     else {
-        return message.substring(0, maxLength) + '...';
+        return message.slice(0, maxLength) + '...';
     }
 }
     console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
